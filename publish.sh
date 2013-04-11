f2cec5dc897e2e1a68bbf3b4485d67bdf2861d4b
leonardfactory <me@leonardfactory.com>
2013-04-12 00:03:40 +0200

    D  CNAME M  atom.xml D  build/404.html D  "build/README 2.md" D  build/archive.html D  build/assets/themes/twitter/bootstrap/css/bootstrap.2.2.2.min.css D  build/assets/themes/twitter/bootstrap/img/glyphicons-halflings-white.png D  build/assets/themes/twitter/bootstrap/img/glyphicons-halflings.png D  build/assets/themes/twitter/css/style.css D  build/atom.xml D  build/categories.html D  build/index.html D  build/lessons/2011/12/29/jekyll-introduction/index.html D  build/pages.html D  build/publish.sh D  build/rss.xml D  build/sitemap.txt D  build/tags.html M  publish.sh M  rss.xml M  tags.html

--- publish.sh
+++ publish.sh
@@ -1,16 +1,17 @@
+#!/bin/bash -e
 # Generate files and push to github
 git checkout master
 git add -A
-git commit
-git push origin master
+git status -s | git commit -F-
+git push origin master || exit
 
-jekyll
+jekyll --no-auto || exit
 git checkout gh-pages
 git rm -qr .
 cp -r _site/. .
 rm -r _site
 git add -A
-git commit
-git push origin gh-pages
+git status -s | git commit -F-
+git push origin gh-pages || exit
 git checkout master
 echo 'Published! You are a nice guy :)'
