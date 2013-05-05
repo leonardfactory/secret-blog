84870f9d1d3563368c708cac3ec349edabe60c60
leonardfactory <me@leonardfactory.com>
2013-05-06 00:14:46 +0200

    M  _config.yml A  push-to-gh-pages.sh

---
+++ push-to-gh-pages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+# Generate files and push to github without updating master
+git checkout master
+
+jekyll --no-auto || exit
+git checkout gh-pages
+git rm -qr .
+cp -r _site/. .
+rm -r _site
+git add -A
+git status -s | git commit -F-
+git push origin gh-pages || exit
+git checkout master
+echo 'Published! You are a nice guy :)'
