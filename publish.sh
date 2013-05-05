1376a2bf1e40e73ded7b06020529c808d381f0bf
leonardfactory <me@leonardfactory.com>
2013-05-06 00:03:29 +0200

    M  publish.sh

--- publish.sh
+++ publish.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 # Generate files and push to github
 git checkout master
+rm -rf _template/css/.sass-cache/
 git add -A
 git status -s | git commit -F- # Use last edits short description to make commit message
 git push origin master || exit
